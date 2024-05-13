2005bc98800a4b9a6ec67d6155bece6099d3395d
Mahardika Shiddiq Anshari <162908160+Mahardikashiddiq@users.noreply.github.com>
2024-05-13 23:34:21 +0700

    Add files via upload

---
+++ loginuas.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+int login();
+int login() {
+    char username[50];
+    char password[50];
+
+    printf("Masukkan nama pengguna: ");
+    scanf("%s", username);
+    printf("Masukkan kata sandi: ");
+    scanf("%s", password);
+
+    if (strcmp(username, "pegawai") == 0 && strcmp(password, "pegawai123") == 0) {
+        return 1;
+    } else {
+        printf("Nama pengguna atau kata sandi salah!\n");
+        return 0;
+    }
+}
+
+int main() {
+    login();
+    return 0;
+}
